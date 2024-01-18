0f18d89a370e6d10ad4d607b8b2d68c5bef9dd12
natarajan-raju <natarajan.raju90@gmail.com>
2024-01-18 16:14:31 +0530

    Logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,8 +8,8 @@ class NavBar extends HTMLElement {
             <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div class="flex lg:flex-1">
                 <a href="#" class="-m-1.5 p-1.5">
-                    <span class="sr-only">BM Wall</span>
-                    <img class="h-8 w-auto border-solid border-2 rounded-full border-red-600" src="../public/images/india.png" alt="BM Wall">
+                    <span class="sr-only">Infinity India</span>
+                    <img class="h-8 w-auto border-solid border-2 rounded-full border-red-600" src="./public/BM_logo.jpg" alt="BM Wall">
                 </a>
                 </div>
                 <div class="flex lg:hidden"> 
