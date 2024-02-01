683b9b2a485d3e57c7981e8232bbf5017ff5d988
natarajan-raju <natarajan.raju90@gmail.com>
2024-02-01 18:11:29 +0530

    V 1.0 ready for production

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -37,7 +37,7 @@ class Footer extends HTMLElement {
                             </ul>
                         </div>
                         <div>
-                            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+                            <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Legal</h2>
                             <ul class="text-gray-500 dark:text-gray-400 font-medium">
                                 <li class="mb-4">
                                     <a href="#" class="hover:underline">Privacy Policy</a>
